e4b545284a8b5038a001cc3bcce462edfd1038e9
Zaracki <119800248+Zaracki@users.noreply.github.com>
2023-05-02 11:15:20 +0200

    fixing HTML and adding jscode

---
+++ js/details.js
@@ -0,0 +1,44 @@
+const queryString = document.location.search;
+
+const params = new URLSearchParams(queryString);
+
+const id = params.get("id");
+
+console.log(id);
+
+const container = document.querySelector(".container");
+const apiUrl = "https://api.noroff.dev/api/v1/square-eyes/" + id;
+
+try {
+  container.innerHTML = "Loading...";
+  async function fetchMovie() {
+  const response = await fetch(apiUrl);
+  const json = await response.json();
+
+  console.log(json);
+
+  
+
+  setTimeout(function(){
+    
+        container.innerHTML = `<section class="product-area">
+  <div class="product-img">
+    <img src="${json.image}" alt="Product picture of Doctor Strange in the Multiverse of Madness">
+  </div>
+  <div class="product-info">
+    <h1>${json.title}</h1>
+    <p>${json.description}</p>
+    <a href="cart.html" class="cta cta-buy">Buy ${json.price}$</a>
+  </div>`;
+
+  document.title = `${json.title}`; 
+  },2000);
+
+  }
+
+   fetchMovie()
+}
+
+catch(error) {
+  
+}
