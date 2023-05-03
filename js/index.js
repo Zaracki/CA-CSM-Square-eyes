1868cfe5bcfd8bd19068f09391367d5a007e235f
Zaracki <119800248+Zaracki@users.noreply.github.com>
2023-05-03 19:19:46 +0200

    fixing error

--- js/index.js
+++ js/index.js
@@ -1,39 +1,36 @@
 const newArrivalsContainer = document.querySelector(".new-arrivals");
 const topPicksContainer = document.querySelector(".top-picks");
 const apiUrl = "https://api.noroff.dev/api/v1/square-eyes/";
+const loadSpinner = document.querySelector(".container");
 
-try {
   
   async function renderHTML() {
-  const response = await fetch(apiUrl);
-  const json = await response.json();
+    loadSpinner.innerHTML = `<div class="lds-dual-ring"></div>`;
+    const response = await fetch(apiUrl);
+    const json = await response.json();
 
-  console.log(json);
+    console.log(json);
 
+    loadSpinner.innerHTML = "";
+    
+    for (let i = 0; i <= 3; i++) {
+      let currentJson = json[i];
 
-  for (let i = 0; i <= 3; i++) {
-    let currentJson = json[i];
+      newArrivalsContainer.innerHTML += `
+      <div>
+      <a href="details.html?id=${currentJson.id}"><img src="${currentJson.image}" class="product_item" alt="${currentJson.title}"></a>
+      </div>`
+    }
 
-    newArrivalsContainer.innerHTML += `
-    <div>
-    <a href="details.html?id=${currentJson.id}"><img src="${currentJson.image}" class="product_item" alt="${currentJson.title}"></a>
-    </div>`
-  }
+    for (let i = 4; i <= 7; i++) {
+      let currentJson = json[i];
 
-  for (let i = 4; i <= 7; i++) {
-    let currentJson = json[i];
-
-    topPicksContainer.innerHTML += `
-    <div>
-    <a href="details.html?id=${currentJson.id}"><img src="${currentJson.image}" class="product_item" alt="${currentJson.title}"></a>
-    </div>`
-  }
+      topPicksContainer.innerHTML += `
+      <div>
+      <a href="details.html?id=${currentJson.id}"><img src="${currentJson.image}" class="product_item" alt="${currentJson.title}"></a>
+      </div>`
+    }
   
   }
   renderHTML()
-}
-catch(error) {
-
-}
-
 
