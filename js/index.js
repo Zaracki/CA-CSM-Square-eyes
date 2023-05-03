09dd7dabc7362d237f1584959a6a52d01591e54f
Zaracki <119800248+Zaracki@users.noreply.github.com>
2023-05-03 19:44:53 +0200

    fixing error and spinner

--- js/index.js
+++ js/index.js
@@ -5,31 +5,38 @@ const loadSpinner = document.querySelector(".container");
 
   
   async function renderHTML() {
-    loadSpinner.innerHTML = `<div class="lds-dual-ring"></div>`;
-    const response = await fetch(apiUrl);
-    const json = await response.json();
+    try {
+      loadSpinner.innerHTML = `<div class="lds-dual-ring"></div>`;
+      const response = await fetch(apiUrl);
+      const json = await response.json();
 
-    console.log(json);
+      console.log(json);
 
-    loadSpinner.innerHTML = "";
-    
-    for (let i = 0; i <= 3; i++) {
-      let currentJson = json[i];
+      loadSpinner.innerHTML = "";
+      
+      for (let i = 0; i <= 3; i++) {
+        let currentJson = json[i];
 
-      newArrivalsContainer.innerHTML += `
-      <div>
-      <a href="details.html?id=${currentJson.id}"><img src="${currentJson.image}" class="product_item" alt="${currentJson.title}"></a>
-      </div>`
+        newArrivalsContainer.innerHTML += `
+        <div>
+        <a href="details.html?id=${currentJson.id}"><img src="${currentJson.image}" class="product_item" alt="${currentJson.title}"></a>
+        </div>`
+      }
+
+      for (let i = 4; i <= 7; i++) {
+        let currentJson = json[i];
+
+        topPicksContainer.innerHTML += `
+        <div>
+        <a href="details.html?id=${currentJson.id}"><img src="${currentJson.image}" class="product_item" alt="${currentJson.title}"></a>
+        </div>`
+      }
     }
 
-    for (let i = 4; i <= 7; i++) {
-      let currentJson = json[i];
-
-      topPicksContainer.innerHTML += `
-      <div>
-      <a href="details.html?id=${currentJson.id}"><img src="${currentJson.image}" class="product_item" alt="${currentJson.title}"></a>
-      </div>`
+    catch(error) {
+      console.log(error);
     }
+
   
   }
   renderHTML()
--- js/products.js
+++ js/products.js
@@ -1,27 +1,32 @@
 const container = document.querySelector(".product_sorted");
+const loadSpinner = document.querySelector(".container");
 const apiUrl = "https://api.noroff.dev/api/v1/square-eyes/";
 
 
-try {
+
   async function renderHTML() {
-  const response = await fetch(apiUrl);
-  const json = await response.json();
+   try {
+    loadSpinner.innerHTML = `<div class="lds-dual-ring"></div>`;
+    const response = await fetch(apiUrl);
+    const json = await response.json();
 
-  console.log(json);
+    console.log(json);
 
-  
-  json.forEach(function(json) {
-    container.innerHTML += `<div>
-    <a href="details.html?id=${json.id}"><img src="${json.image}" class="product_item product_item-big" alt="${json.title}"></a>
-  </div>`
+    loadSpinner.innerHTML = "";
+    
+    json.forEach(function(json) {
+      container.innerHTML += `<div>
+      <a href="details.html?id=${json.id}"><img src="${json.image}" class="product_item product_item-big" alt="${json.title}"></a>
+    </div>`
 
-  })
+    })
+   }
+
+   catch(error) {
+    console.log(error);
+   }
+ 
 
 }
 
 renderHTML()
-}
-
-catch(error) {
-
-}
