1f8aea5b87677d3f60626940a0af9ade923aaa61
Zaracki <119800248+Zaracki@users.noreply.github.com>
2023-05-03 12:42:24 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,12 +1,15 @@
 const container = document.querySelector(".product_sorted");
 const apiUrl = "https://api.noroff.dev/api/v1/square-eyes/";
 
-async function fetchMovie() {
+
+try {
+  async function renderHTML() {
   const response = await fetch(apiUrl);
   const json = await response.json();
 
   console.log(json);
 
+  
   json.forEach(function(json) {
     container.innerHTML += `<div>
     <a href="details.html?id=${json.id}"><img src="${json.image}" class="product_item product_item-big" alt="${json.title}"></a>
@@ -16,4 +19,9 @@ async function fetchMovie() {
 
 }
 
-fetchMovie()
+renderHTML()
+}
+
+catch(error) {
+
+}
