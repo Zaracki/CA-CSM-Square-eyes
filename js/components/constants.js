86d559f0dac38e432d19d823619c9ad73abfb281
Zaracki <119800248+Zaracki@users.noreply.github.com>
2023-08-21 14:20:39 +0200

    Update constants.js

--- js/components/constants.js
+++ js/components/constants.js
@@ -1,2 +1,2 @@
-export const apiUrl = "https://www.a33.no/wp-json/wc/store/products";
+export const apiUrl = "https://cors.noroff.dev/a33.no/wp-json/wc/store/products";
 export const spinnerDiv = `<div class="lds-dual-ring"></div>`;
