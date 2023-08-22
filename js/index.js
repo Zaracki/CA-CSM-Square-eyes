7f2dd8af00a81ac2967878fa33a915ca7113c009
Zaracki <119800248+Zaracki@users.noreply.github.com>
2023-08-22 20:04:44 +0200

    Updating

--- js/index.js
+++ js/index.js
@@ -11,18 +11,8 @@ const loadingDiv = document.querySelector(".loadingContainer");
       const response = await fetch(apiUrl);
       const json = await response.json();
 
-
       loadingDiv.innerHTML = "";
       
-      for (let i = 0; i <= 3; i++) {
-        let currentJson = json[i];
-
-        newArrivalsContainer.innerHTML += `
-        <div>
-        <a href="details.html?id=${currentJson.id}"><img src="${currentJson.images[0].src}" class="product_item" alt="${currentJson.title}"></a>
-        </div>`
-      }
-
       for (let i = 4; i <= 7; i++) {
         let currentJson = json[i];
 
@@ -36,8 +26,32 @@ const loadingDiv = document.querySelector(".loadingContainer");
     catch(error) {
       loadingDiv.innerHTML = displayError();
     }
-
-  
   }
+
+  async function renderFeatured(){
+    try {
+      loadingDiv.innerHTML = spinnerDiv;
+      const featuredUrl = apiUrl + "?featured=true"
+      const response = await fetch(featuredUrl);
+      const json = await response.json();
+
+      loadingDiv.innerHTML = "";
+
+      for (var movie of json) {
+
+
+        newArrivalsContainer.innerHTML += `
+        <div>
+        <a href="details.html?id=${movie.id}"><img src="${movie.images[0].src}" class="product_item" alt="${movie.name}"></a>
+        </div>`
+      }
+    }
+
+    catch(error) {
+      loadingDiv.innerHTML = displayError();
+    }
+  }
+
+  renderFeatured()
   renderHTML()
 
